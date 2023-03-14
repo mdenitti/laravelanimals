b7dde505845e0e7d01e7d7ad0718143377e7659d
mdenitti <massimodn77@gmail.com>
2023-03-14 14:20:35 +0100

    Added Alien Model and replaced the DB facade with a Eloquent Model approach

--- app/Http/Controllers/UfoController.php
+++ app/Http/Controllers/UfoController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Alien;
 
 class UfoController extends Controller
 {
     public function show() {
-        $aliens = DB::table('aliens')->get();
+        //$aliens = DB::table('aliens')->get();
+        $aliens = Alien::all();
         //dd($aliens);
         return view('aliens.home', compact('aliens'));
     }
